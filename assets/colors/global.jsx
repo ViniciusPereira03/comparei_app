e3df584a2009afc9a11e3f376f74b33ed651a51c
Vinícius Pereira <vinicius.gpereira03@gmail.com>
2025-01-14 20:42:25 -0300

    Criada constante global de cores

---
+++ assets/colors/global.jsx
@@ -0,0 +1,18 @@
+export const colors = {
+    white: "#FFF",
+    mint_cream: "#EFFAF6",
+    mint_green: "#DEF5EC",
+    mint_green_2: "#BCEAD9",
+    turquoise: "#78D5B3",
+    hookers_green: "#3C6B5A",
+    hookers_green_opacity: "#3c6b5ae6",
+    dark_green: "#1E362D",
+    black: "#000",
+    xanthous: "#E6AF2E",
+    tomato: "#FA694B",
+    coquelicot: "#F9502D",
+    scarlet: "#F8370F",
+    bronze: "#CD7F32",
+    silver: "#C0C0C0",
+    gold: "#FFD700"
+}
